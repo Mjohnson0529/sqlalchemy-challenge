fd6b5dd897c8aa0073687f81134e7393e6f2351a
Mjohnson0529 <ma206505@ucf.edu>
2024-09-23 17:01:40 -0400

    final commit

--- Module 10 challenge/app.py
+++ Module 10 challenge/app.py
@@ -1,4 +1,12 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 
@@ -8,20 +16,23 @@
 
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
+station = Base.classes.station
+measurement = Base.classes.measurement
 
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
